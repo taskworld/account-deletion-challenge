c8b6c0522fdf8edc16bc60f4ab1c2db4753d0324
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2019-11-06 13:30:00 +0700

    Removed "checkOwnership" endpoint for simplicity

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
 module.exports.fetchWorkspaces = require('./fetchWorkspaces')
-module.exports.checkOwnership = require('./checkOwnership')
 module.exports.submitSurvey = require('./submitSurvey')
 module.exports.terminateAccount = require('./terminateAccount')
